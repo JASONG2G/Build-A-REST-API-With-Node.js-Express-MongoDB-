93fdcfd4ed0249feccab27dcd6eb57bbead35d27
jasonxjung <jasonjung.xc@gmail.com>
2020-04-28 12:20:22 +0300

    Added middleware function getSubscriber and completed update and delete route, tutorial completed

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,8 +13,8 @@ router.get('/', async (req, res) => {
 })
 
 //Getting one 
-router.get('/:id', (req, res) => {
-  res.send(req.params.id)
+router.get('/:id', getSubscriber, (req, res) => {
+  res.send(res.subscriber)
 })
 //Creating one
 router.post('/', async (req, res) => {
@@ -31,12 +31,48 @@ router.post('/', async (req, res) => {
   }
 })
 //Updating one
-router.patch('/', (req, res) => {
-  
+router.patch('/:id', getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch (error) {
+    res.status(400).json({
+      message: error.mesage
+    })
+    
+  }
 })
 //Deleting one
-router.delete('/:id', (req, res) => {
-  
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove()
+    res.json({ message: 'Deleted Subscriber'})
+  } catch (error) {
+    res.status(500),json({ message: error.mesage })
+  }
 })
+
+//middleware function 
+async function getSubscriber(req, res, next) {
+  let subscriber
+try {
+  subscriber = await Subscriber.findById(req.params.id)
+  if (subscriber == null ){
+    return res.status(404).json({ mesage: 'cannot find subscriber'})
+  }
+} catch (error) {
+  return res.status(500).json({ message: error.message})
+}
+
+res.subscriber = subscriber 
+next()
+}
+
 
 module.exports = router
